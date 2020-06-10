d8e62650b079d95bf75395aa9415e8fdd59fb089
Glitch (hello-webpage) <none>
2020-06-10 00:30:22 +0000

    🚓🎢 Checkpoint ./script.js:3892952/1902

--- script.js
+++ script.js
@@ -4,5 +4,16 @@
 // prints "hi" in the browser's dev tools console
 console.log("working");
 
-let square1 = docuquerySelector#aa;
+let rw1cl1 = document.querySelector(".aa");
+let rw1cl2 = document.querySelector(".ab");
+let rw1cl3 = document.querySelector(".ac");
+let rw2cl1 = document.querySelector(".ba");
+let rw2cl2 = document.querySelector(".bb");
+let rw2cl3 = document.querySelector(".bc");
+let rw3cl1 = document.querySelector(".ca");
+let rw3cl2 = document.querySelector(".cb");
+let rw3cl3 = document.querySelector(".cc");
 
+rw1cl1.addEventListener("click", e => {
+  console.log("#1 clicked");
+});
