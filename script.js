83901284c45b8ddb34b6cadcdfa2c8b5ca2556b2
Glitch (hello-webpage) <none>
2020-06-10 01:40:21 +0000

    👬😬 Checkpoint ./script.js:3892952/1185

--- script.js
+++ script.js
@@ -6,24 +6,44 @@ console.log("working");
 
 let rw1cl1 = document.querySelector("#soda");
 let rw1cl2 = document.querySelector("#salad");
-let rw1cl3 = document.querySelector("#friedc");
-let rw2cl1 = document.querySelector("#ba");
-let rw2cl2 = document.querySelector("#bb");
-let rw2cl3 = document.querySelector("#bc");
-let rw3cl1 = document.querySelector("#ca");
-let rw3cl2 = document.querySelector("#cb");
-let rw3cl3 = document.querySelector("#cc");
-let check = document.querySelector("#check")
+let rw1cl3 = document.querySelector("#chicken");
+let rw2cl1 = document.querySelector("#burger");
+let rw2cl2 = document.querySelector("#fries");
+let rw2cl3 = document.querySelector("#hotdog");
+let rw3cl1 = document.querySelector("#mustard");
+let rw3cl2 = document.querySelector("#ketchup");
+let rw3cl3 = document.querySelector("#shake");
+let check = document.querySelector("#check");
 let sum=0
 
 rw1cl1.addEventListener("click", (e) => {
-  check.classList.add("#aa")
+  check.classList.add("#soda")
   console.log("#1 clicked");
-});
+})
 
-console.log(`You ordered.` + check.classList + `Your total is` + sum)
-if () {
-  
-} elese {
-  
-}
+// rw1cl1.addEventListener("click", (e) => {
+//   check.classList.add("#salad")
+//   console.log("#1 clicked");
+// });rw1cl1.addEventListener("click", (e) => {
+//   check.classList.add("#chicken")
+//   console.log("#1 clicked");
+// });rw1cl1.addEventListener("click", (e) => {
+//   check.classList.add("#burger")
+//   console.log("#1 clicked");
+// });rw1cl1.addEventListener("click", (e) => {
+//   check.classList.add("#fries")
+//   console.log("#1 clicked");
+// });rw1cl1.addEventListener("click", (e) => {
+//   check.classList.add("#hotdog")
+//   console.log("#1 clicked");
+// });rw1cl1.addEventListener("click", (e) => {
+//   check.classList.add("#mustard")
+//   console.log("#1 clicked");
+// });rw1cl1.addEventListener("click", (e) => {
+//   check.classList.add("#ketchup")
+//   console.log("#1 clicked");
+// });rw1cl1.addEventListener("click", (e) => {
+//   check.classList.add("#shake")
+//   console.log("#1 clicked");
+// });
+
