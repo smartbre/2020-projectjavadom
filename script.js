1a107bd527292cb7a6ad5b500506ada46b45b1da
Glitch (hello-webpage) <none>
2020-06-10 00:41:22 +0000

    🏃🛍 Checkpoint ./script.js:3892952/294 ./index.html:3892952/67

--- script.js
+++ script.js
@@ -7,13 +7,9 @@ console.log("working");
 let rw1cl1 = document.querySelector(".aa");
 let rw1cl2 = document.querySelector(".ab");
 let rw1cl3 = document.querySelector(".ac");
-let rw2cl1 = document.querySelector(".ba");
-let rw2cl2 = document.querySelector(".bb");
-let rw2cl3 = document.querySelector(".bc");
-let rw3cl1 = document.querySelector(".ca");
-let rw3cl2 = document.querySelector(".cb");
-let rw3cl3 = document.querySelector(".cc");
 
-rw1cl1.addEventListener("click", e => {
+
+rw1cl1.addEventListener("click", (e) => {
+  rw1c1.innerHMTL=
   console.log("#1 clicked");
 });
