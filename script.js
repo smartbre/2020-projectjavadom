d0dde311303458c2742068d694d9726513cd89a8
Glitch (hello-webpage) <none>
2020-06-10 01:52:21 +0000

    🕷🌋 Checkpoint ./script.js:3892952/2684 ./index.html:3892952/11

--- script.js
+++ script.js
@@ -4,46 +4,69 @@
 // prints "hi" in the browser's dev tools console
 console.log("working");
 
-let rw1cl1 = document.querySelector("#soda");
-let rw1cl2 = document.querySelector("#salad");
-let rw1cl3 = document.querySelector("#chicken");
-let rw2cl1 = document.querySelector("#burger");
-let rw2cl2 = document.querySelector("#fries");
-let rw2cl3 = document.querySelector("#hotdog");
-let rw3cl1 = document.querySelector("#mustard");
-let rw3cl2 = document.querySelector("#ketchup");
-let rw3cl3 = document.querySelector("#shake");
-let check = document.querySelector("#check");
+let rw1cl1 = document.querySelector("#soda")
+let rw1cl2 = document.querySelector("#salad")
+let rw1cl3 = document.querySelector("#chicken")
+let rw2cl1 = document.querySelector("#burger")
+let rw2cl2 = document.querySelector("#fries")
+let rw2cl3 = document.querySelector("#hotdog")
+let rw3cl1 = document.querySelector("#mustard")
+let rw3cl2 = document.querySelector("#ketchup")
+let rw3cl3 = document.querySelector("#shake")
+let check = document.querySelector("#check")
 let sum=0
 
-rw1cl1.addEventListener("click", (e) => {
+rw1cl1.addEventListener("click", e => {
   check.classList.add("#soda")
-  console.log("#1 clicked");
+  console.log("#soda clicked")
+  console.log(check.classList)
 })
 
-// rw1cl1.addEventListener("click", (e) => {
-//   check.classList.add("#salad")
-//   console.log("#1 clicked");
-// });rw1cl1.addEventListener("click", (e) => {
-//   check.classList.add("#chicken")
-//   console.log("#1 clicked");
-// });rw1cl1.addEventListener("click", (e) => {
-//   check.classList.add("#burger")
-//   console.log("#1 clicked");
-// });rw1cl1.addEventListener("click", (e) => {
-//   check.classList.add("#fries")
-//   console.log("#1 clicked");
-// });rw1cl1.addEventListener("click", (e) => {
-//   check.classList.add("#hotdog")
-//   console.log("#1 clicked");
-// });rw1cl1.addEventListener("click", (e) => {
-//   check.classList.add("#mustard")
-//   console.log("#1 clicked");
-// });rw1cl1.addEventListener("click", (e) => {
-//   check.classList.add("#ketchup")
-//   console.log("#1 clicked");
-// });rw1cl1.addEventListener("click", (e) => {
-//   check.classList.add("#shake")
-//   console.log("#1 clicked");
-// });
+rw1cl2.addEventListener("click", e => {
+  check.classList.add("#salad")
+  console.log("salad clicked")
+  console.log(check.classList)
+})
+
+rw1cl3.addEventListener("click", e => {
+ check.classList.add("#chicken")
+console.log("chicken clicked")
+  console.log(check.classList)
+ })
+
+rw2cl1.addEventListener("click", (e) => {
+   check.classList.add("#burger")
+   console.log("#urger clicked");
+  console.log(check.classList)
+ })
+
+rw2cl2.addEventListener("click", (e) => {
+   check.classList.add("#fries")
+   console.log("fries clicked");
+  console.log(check.classList)
+ })
+
+rw2cl3.addEventListener("click", (e) => {
+   check.classList.add("#hotdog")
+   console.log("hotdog clicked")
+  console.log(check.classList)
+ })
+
+rw3cl1.addEventListener("click", (e) => {
+   check.classList.add("#mustard")
+   console.log("mustard clicked");
+  console.log(check.classList)
+ })
+
+rw3cl2.addEventListener("click", (e) => {
+   check.classList.add("#ketchup")
+   console.log("ketchup clicked");
+  console.log(check.classList)
+ })
+
+rw3cl3.addEventListener("click", (e) => {
+   check.classList.add("#shake")
+   console.log("shake clicked");
+  console.log(check.classList)
+})
 
