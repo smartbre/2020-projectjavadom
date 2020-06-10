36777ec375a9986cb331163fb184f146876f8f74
Glitch (hello-webpage) <none>
2020-06-10 01:17:21 +0000

    🖼🍝 Checkpoint ./script.js:3892952/450

--- script.js
+++ script.js
@@ -7,9 +7,23 @@ console.log("working");
 let rw1cl1 = document.querySelector(".aa");
 let rw1cl2 = document.querySelector(".ab");
 let rw1cl3 = document.querySelector(".ac");
-
+let rw2cl1 = document.querySelector(".ba");
+let rw2cl2 = document.querySelector(".bb");
+let rw2cl3 = document.querySelector(".bc");
+let rw3cl1 = document.querySelector(".ca");
+let rw3cl2 = document.querySelector(".cb");
+let rw3cl3 = document.querySelector(".cc");
+let check = document.querySelector(".check")
+let sum=0
 
 rw1cl1.addEventListener("click", (e) => {
   rw1c1.innerHMTL=
   console.log("#1 clicked");
 });
+
+console.log(`You ordered.` + check.classList + `Your total is` + sum)
+if () {
+  
+} elese {
+  
+}
